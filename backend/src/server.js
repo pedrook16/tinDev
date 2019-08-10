82313351fab279bb8ce8c5caa0713e9d9dc098c6
Pedro <pedrook16@gmail.com>
2019-08-10 13:43:11 -0300

    init

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+
+
+const server = express()
+mongoose.connect('mongodb+srv://pedrook16:ba0896@P@cluster0-iubz8.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
+
+server.use(cors())
+server.use(express.json())
+server.use(routes)
+
+server.listen(5000)
