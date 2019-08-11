442f931e56fbf747c1a663d81199674ebdeac728
Pedro <pedrook16@gmail.com>
2019-08-11 12:18:50 -0300

    add

---
+++ mobile/tindev/src/index.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './routes'
+
+
+export default function App() {
+  return (
+    <Routes/>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
+
